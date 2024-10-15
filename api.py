4076ade4891a3001f4644d4017feb09bef2390e2
Rohith Kyla <rohithkyla@gmail.com>
2024-10-15 11:58:17 +0000

    chore: add only relevant details from the datasets api

--- api.py
+++ api.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 
 @app.route('/search-datasets', methods=['GET'])
 def search_datasets():
-    # Logic to fetch datasets from HFDatasets or other sources
     response = requests.get('https://huggingface.co/api/datasets')
-    return jsonify(response.json())
+    datasets = response.json()
+    
+    relevant_details = []
+    for dataset in datasets:
+        relevant_details.append({
+            'id': dataset['id'],
+            'author': dataset['author'],
+            'description': dataset['description'][:200] + '...' if len(dataset['description']) > 200 else dataset['description'],
+            'downloads': dataset['downloads'],
+            'likes': dataset['likes'],
+            'tags': dataset['tags']
+        })
+    
+    return jsonify(relevant_details)
 
 # Add more routes for preprocessing, model selection, and finetuning
 
